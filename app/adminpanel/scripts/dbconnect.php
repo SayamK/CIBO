a3d05fe7bf574e014340f931e20189e5ea99a72a
Sayam Kanwar <sayamkanwar616@gmail.com>
2018-02-04 19:34:06 +0530

    Update dbconnect.php

--- app/adminpanel/scripts/dbconnect.php
+++ app/adminpanel/scripts/dbconnect.php
@@ -1,18 +1,13 @@
 <?php
-	$username = "sayaitoe_admin";
-	$password = "buzodog115";
+	$username = **Username**;
+	$password = **Password**;
 	$hostname = "localhost";
-	$database = "sayaitoe_cibo";
-
+	$database = **DB_Name**;
 	ini_set('session.gc_maxlifetime', 3600*3);
 	//Connect to DB.
 	$dbhandle = mysql_connect($hostname, $username, $password)
   		or die("Unable to connect to MySQL");
-
-
   	$selected_db = mysql_select_db($database,$dbhandle)
  		 or die("Could not select examples");
-
- 		  // error_reporting(0);
-
+ 		  error_reporting(0);
 ?>
